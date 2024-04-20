b832c167d95f1e8e23ba5636d751358dc7c1ea6f
pavantn36918 <148556331+pavantn36918@users.noreply.github.com>
2024-04-20 12:29:13 +0530

    Update 21schcgpa.js

--- 21schcgpa.js
+++ 21schcgpa.js
@@ -5,21 +5,22 @@ function calculate() {
     let s2 = parseFloat(document.getElementById("s2").value);
     let s3 = parseFloat(document.getElementById("s3").value);
     let s4 = parseFloat(document.getElementById("s4").value);
+    let s5 = parseFloat(document.getElementById("s5").value);
 
     let a = [];
-    let b = [s1, s2, s3, s4];
-    let c = [20, 20, 18, 22];
+    let b = [s1, s2, s3, s4,s5];
+    let c = [20, 20, 18, 22,18];
     let sum = 0;
 
     let result = document.getElementById("result");
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 5; j++) {
 
         a[j] = b[j] * c[j];
     }
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 5; i++) {
         sum = sum + a[i];
 
     }
-    let res = sum / 80;
+    let res = sum / 98;
     result.innerHTML = res.toFixed(2);
 }
