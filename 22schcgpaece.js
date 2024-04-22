cbd4a87dd269021bc9fabbd43fff4012c4429834
pavantn36918 <148556331+pavantn36918@users.noreply.github.com>
2024-04-22 13:01:51 +0530

    Add files via upload

---
+++ 22schcgpaece.js
@@ -0,0 +1,22 @@
+function calculate() {
+    let s1 = parseFloat(document.getElementById("s1").value);
+    let s2 = parseFloat(document.getElementById("s2").value);
+    let s3 = parseFloat(document.getElementById("s3").value);
+
+    let a = [];
+    let b = [s1, s2, s3];
+    let c = [20, 20, 19];
+    let sum = 0;
+
+    let result = document.getElementById("result");
+    for (let j = 0; j < 3; j++) {
+
+        a[j] = b[j] * c[j];
+    }
+    for (let i = 0; i < 3; i++) {
+        sum = sum + a[i];
+
+    }
+    let res = sum / 59;
+    result.innerHTML = res.toFixed(2);
+}
